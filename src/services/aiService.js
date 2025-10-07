cef0fb651a032cc5fd47733f393088ac359f879e
harshitasuryawanshi2003 <harshitasuryawanshi2003@gmail.com>
2025-10-07 19:01:13 +0530

    Updated

--- src/services/aiService.js
+++ src/services/aiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = `https://health-news-curator-backend.onrender.com`;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 export async function generateSummary(articleText, language) {
   const response = await axios.post(`${BACKEND_URL}/api/ai/generate-summary`, { articleText,language });
